bfe049a64ae0f15a2a54210828ff0bf801228d64
Michael Wurster <miwurster@googlemail.com>
2022-06-10 14:11:44 +0200

    feat: bump version

--- generators/app/index.js
+++ generators/app/index.js
@@ -21,7 +21,7 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 const helper = require('./helper');
 
-const RELEASED_VERSION = 'v1.26.0';
+const RELEASED_VERSION = 'v1.26.2';
 
 module.exports = class extends Generator {
   constructor(args, opts) {
